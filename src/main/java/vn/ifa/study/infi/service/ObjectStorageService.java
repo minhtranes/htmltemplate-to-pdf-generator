2acb2ef5a07f2ab481594f5275fa05bfb7a1222b
Minh Trần <minh.tran@spsglobal.com>
2022-12-10 16:04:40 +0700

    Create bucket if needed

--- src/main/java/vn/ifa/study/infi/service/ObjectStorageService.java
+++ src/main/java/vn/ifa/study/infi/service/ObjectStorageService.java
@@ -8,6 +8,8 @@ import java.security.NoSuchAlgorithmException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.minio.BucketExistsArgs;
+import io.minio.MakeBucketArgs;
 import io.minio.MinioClient;
 import io.minio.PutObjectArgs;
 import io.minio.errors.ErrorResponseException;
@@ -16,9 +18,11 @@ import io.minio.errors.InternalException;
 import io.minio.errors.InvalidResponseException;
 import io.minio.errors.ServerException;
 import io.minio.errors.XmlParserException;
+import lombok.extern.slf4j.Slf4j;
 import vn.ifa.study.infi.model.RemoteFile;
 import vn.ifa.study.infi.properties.ObjectStorageProperties;
 
+@Slf4j
 @Service
 public class ObjectStorageService {
 
@@ -56,12 +60,25 @@ public class ObjectStorageService {
             throws InvalidKeyException, ErrorResponseException, InsufficientDataException, InternalException,
             InvalidResponseException, NoSuchAlgorithmException, ServerException, XmlParserException, IOException {
 
+        boolean found = minioClient().bucketExists(BucketExistsArgs.builder()
+                .bucket(bucket)
+                .build());
+
+        if (!found) {
+            minioClient().makeBucket(MakeBucketArgs.builder()
+                    .bucket(bucket)
+                    .build());
+            log.info("Bucket [{}] has been created", bucket);
+        }
+
         PutObjectArgs oa = PutObjectArgs.builder()
                 .stream(is, size, -1)
                 .bucket(bucket)
                 .object(name)
                 .build();
         minioClient().putObject(oa);
+        log.info("Put object [{}] to bucket [{}] successfully", name, bucket);
+
         return RemoteFile.builder()
                 .filename(name)
                 .url(String.join("/", bucket, name))
